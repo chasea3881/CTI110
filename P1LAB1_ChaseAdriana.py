399388d55643f31cf5bc534bf24d4899a846557d
chasea3881 <chasea3881@student.faytechcc.edu>
2025-06-15 12:47:04 -0400

    Addeds P1LAB1 Homework Assignment

---
+++ P1LAB1_ChaseAdriana.py
@@ -0,0 +1,11 @@
+ # Adriana Chase
+ # 6/15/2025
+ # P1LAB1
+ # User Input and Output
+
+print("Enter Your First Name: ", end = " ")
+first =  input()
+print("Enter Your Last Name: ", end = " ")
+last =  input() 
+print("Hello, " + first + " " + last + " " + "! Welcome to CTI-110")
+ 
